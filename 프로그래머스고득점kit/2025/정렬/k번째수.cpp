624d87d4df84a7b2c45998065104663ac47c6ea5
candosh <csh200310@naver.com>
2025-01-18 18:07:25 +0900

    k번째수

---
+++ 프로그래머스고득점kit/2025/정렬/k번째수.cpp
@@ -0,0 +1,24 @@
+// 슬라이싱 하고 싶을 때 for 돌려서 push_back 하기!
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands)
+{
+	vector<int> answer;
+	for (int i = 0; i < commands.size(); i++)
+	{
+		vector<int> tmp;
+		for (int j = commands[i][0] - 1; j <= commands[i][1] - 1; j++)
+		{
+			tmp.push_back(array[j]);
+		}
+		sort(tmp.begin(), tmp.end());
+		answer.push_back(tmp[commands[i][2] - 1]);
+	}
+
+	return answer;
+}
